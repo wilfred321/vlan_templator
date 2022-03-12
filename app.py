47c389bbbfe3aad7ca3209b5349c3c06d82e7130
[Wilfred Owobu] <owobuwilfred@gmail.com>
2022-03-12 10:30:14 -0500

    added upload file

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from vlan_tmpl import create_app
 app = create_app()
 app.config['TEMPLATES_AUTO_RELOAD'] = True 
 app.config['SECRET_KEY'] = 'Thisisaverysecretkey'
-app.config['UPLOAD_FOLDER'] = ''
+app.config['UPLOAD_FOLDER'] = './vlan_tmpl/static/files'
 
 
